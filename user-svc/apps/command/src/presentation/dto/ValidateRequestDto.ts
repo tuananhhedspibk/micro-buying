88526cf0b891895515f8c77a25e3df90417be757
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2023-10-09 18:46:53 +0900

    :wastebasket: Remove user svc

--- user-svc/apps/command/src/presentation/dto/ValidateRequestDto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString } from 'class-validator';
-
-import { ValidateRequest } from '../../domain/proto/auth.pb';
-
-export class ValidateRequestDto implements ValidateRequest {
-  @IsString()
-  public readonly token: string;
-}
