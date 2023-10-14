e552b443ac8474cd621ccf72bf38f2dc6bcb321b
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2023-10-14 16:14:28 +0900

    :label: Constants for kafka

---
+++ item-svc/apps/shared/kafka/constants.ts
@@ -0,0 +1,11 @@
+export const ClientId = {
+  Item: 'Item',
+} as const;
+
+export const ConsumerGroupId = {
+  ItemSvc: 'ItemSvc',
+} as const;
+
+export const MessagePattern = {
+  ItemCreatedEvent: 'ItemCreatedEvent',
+};
