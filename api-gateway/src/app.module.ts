c758dfb543a3dd2883521a4693246cc69db68377
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2023-10-14 16:18:02 +0900

    :heavy_plus_sign: Item proto install command

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ItemModule],
 })
 export class AppModule {}
